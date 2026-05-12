7fb83bb47131e95df295afa5b6cf04473e58cc1c
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-05-12 15:53:30 -0700

    update spfk-metadata-base to 1.0.2

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ryanfrancesconi/spfk-taglib", from: "1.0.0"),
         .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", from: "0.0.4"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-metadata-base", from: "0.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-metadata-base", from: "1.0.2"),
         .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.4"),
         .package(url: "https://github.com/ryanfrancesconi/spfk-utils", from: "0.0.8"),
         .package(url: "https://github.com/sbooth/ogg-binary-xcframework", from: "0.1.3"),
